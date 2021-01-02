f2aa04ea5382cb7520e67b900029987cad166ac8
khangjacky <khangjacky@outlook.com>
2021-01-02 21:48:58 +0800

    Add auth

--- tiddlywiki/init-and-run.sh
+++ tiddlywiki/init-and-run.sh
@@ -4,4 +4,4 @@ if [ ! -f "/app/tiddlywiki.info" ]; then
     tiddlywiki /app --init server
 fi
 
-tiddlywiki /app --listen host=0.0.0.0
+tiddlywiki /app --listen host=0.0.0.0 username=${TW_USERNAME} password=${TW_PASSWORD}
